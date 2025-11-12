5abf78f05faab4a98ccbe8da1e30cfdbcce75816
prikshitTech <prikshit.jaswal@auspicioussoft.in>
2025-11-12 12:00:09 +0530

    Final Push

--- src/config/stripe.ts
+++
@@ -1,9 +0,0 @@
-import { configDotenv } from 'dotenv';
-import Stripe from 'stripe';
-configDotenv()
-
-const apiVersion : any = '2025-05-28.basil';
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {apiVersion});
-
-export default stripe
--- src/routes/index.ts
+++
@@ -1,4 +0,0 @@
-export {router as admin} from './admin-routes'
-export {router as user} from './user-routes'
-
-export {router as auth} from './auth-routes'
